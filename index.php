078335ef40156a3596fd1ad43df0d14ad3ccdbd9
Higor <delimadasilvaahigor@gmail.com>
2026-09-14 21:36:23 -0300

    Adiciona seção sobre o projeto

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
  <ul>
  <li>Nome do integrante A</li>
  <li>Nome do integrante B</li>
+ <h2>Sobre o projeto</h2>
+<p>
+ Este projeto demonstra o desenvolvimento colaborativo
+ utilizando Git e GitHub.
+</p>
  </ul>
 </body>
 </html>
